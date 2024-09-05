d879c5f3a7e381dfe6bcad1fce69316694c2a6fb
Kevin Mahusay <kevinmahusay22@gmail.com>
2024-09-06 05:35:19 +0800

    second commit 09/06/2024

--- app/src/main/java/com/mab/calculator_application/MainActivity.java
+++ app/src/main/java/com/mab/calculator_application/MainActivity.java
@@ -7,11 +7,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import java.text.DecimalFormat;
 
-import androidx.activity.EdgeToEdge;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
 
     private TextView resultTextView;
